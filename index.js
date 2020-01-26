7cc68878b1ae9f2331bb7c88b498aec23a0d7327
Khris Punzalan <khrispunzalan@gmail.com>
2020-01-25 16:27:50 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-
 let totalBatteries = batteryBatches.reduce(function(total, element){return element + total}, 0)
+
+
+// arr.reduce(callback( accumulator, currentValue[, index[, array]] )[, initialValue])
+
+// accumulator
+// The accumulator accumulates callback's return values. It is the accumulated value previously returned in the last invocation of the callback—or initialValue, if it was supplied (see below).
+// currentValue
+// The current element being processed in the array.
